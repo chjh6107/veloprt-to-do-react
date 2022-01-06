5e7dfeea134574c969a45925727ab5ce3c613545
chjh6107 <chjh6107@gmail.com>
2022-01-06 23:08:08 +0900

    zero부터 다시시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
-import { TodoProvider } from "./components/TodoContext";
-import TodoCreate from "./components/TodoCreate";
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
 import TodoTemplate from "./components/TodoTemplate"
+
 const App=()=> {
 
     return (
-        <TodoProvider>
-            <TodoTemplate>
-                <TodoHead/>
-                <TodoList/>
-                <TodoCreate/>
-            </TodoTemplate>
-        </TodoProvider>
+        <TodoTemplate>
+            <div>1234</div>
+            <div>432</div>
+            <div>내 컴퓨터 누가 해킹했어~</div>
+            <div>살려달라고 빌어봐~</div>
+            <div>제발 살려줘~
+            </div>
+        </TodoTemplate>
     )
 }
 
--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -4,25 +4,14 @@ const TodoTemplateBlock = styled.div`
     width: 512px;
     height: 768px;
 
-    // 박수 하단에 추가 버튼을 위치시키기 위한 설정
-    position: relative;
-    background: white;
-    border-radius: 16px;
-    box-shadow: 0 0 12px 0px rgba(0,0,0,.3);
-
-    margin: 0 auto;
-
-    margin-top: 96px;
-    margin-bottom: 32px;
-    display: flex; //유동적인 레이아웃을 만들기 위한 것
-    flex-direction: column;
+    display: flex;
+    flex-direction: row;
+    /* flex-direction: column-reverse; */
+    /* align-items: center; */
 `;
 
-const TodoTemplate= ({children}) =>{
-    return(
-        <TodoTemplateBlock>
-            {children}
-        </TodoTemplateBlock>
-    )
+const TodoTemplate = ({children}) =>{
+    return <TodoTemplateBlock>{children}</TodoTemplateBlock>
 }
+
 export default TodoTemplate;
